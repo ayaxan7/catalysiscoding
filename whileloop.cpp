0e8029edd0f691f4ab284b7efd56d34d9b80484c
SnehalPrakash <136785552+SnehalPrakash@users.noreply.github.com>
2023-12-19 14:54:51 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n,i=1;
+    cout<<"Enter the last number"<<endl;
+    cin>>n;
+    cout<<"natural numbers till that number"<<endl;
+
+    while (i<n) 
+    {
+        cout<<i<<endl;
+        i++;
+    }
+
+    return 0;
+}

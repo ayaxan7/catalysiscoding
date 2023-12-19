c4ca56ee35e84c461689d98ccfbd9057b6ee2a53
SnehalPrakash <136785552+SnehalPrakash@users.noreply.github.com>
2023-12-19 14:47:09 +0530

    Add files via upload

---
+++ forloops.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<i<<endl;
+        
+    
+        /*       syntax of for loop
+        for(initialisation;condition,updation)
+        {
+            loop body(c++ code)
+        }
+        */
+       //careful use infinite loops
+    }
+    
+    return 0;
+
+}
